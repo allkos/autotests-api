253f39e2f827c6141505a8b121e335a5ca4de259
allkos <a.s.kosarev@yandex.ru>
2025-08-31 15:21:58 +0300

    Фикстуры Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,7 @@
 import pytest
 from http import HTTPStatus
 
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
@@ -10,8 +10,7 @@ from tools.assertions.users import assert_create_user_response
 
 @pytest.mark.users  # Добавили маркировку users
 @pytest.mark.regression  # Добавили маркировку regression
-def test_create_user():
-    public_users_client = get_public_users_client()
+def test_create_user(public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
 
     request = CreateUserRequestSchema()
     response = public_users_client.create_user_api(request)
