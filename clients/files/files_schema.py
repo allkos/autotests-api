bc9adce8befb7cbb614690a991cb94885edfe782
allkos <a.s.kosarev@yandex.ru>
2025-09-01 19:53:29 +0300

    Практикуемся в написании API автотестов. Часть 1

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -29,3 +29,9 @@ class CreateFileResponseSchema(BaseModel):
     Описание структуры ответа создания файла.
     """
     file: FileSchema
+
+class GetFileResponseSchema(BaseModel):
+    """
+    Описание структуры запроса получения файла.
+    """
+    file: FileSchema
