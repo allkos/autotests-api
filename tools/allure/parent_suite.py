a505d53405233dadb608703bedc626fd5db81726
allkos <a.s.kosarev@yandex.ru>
2025-10-26 15:29:04 +0200

    Интеграция Allure в автотесты.Практическая работа с Allure аннотациями

--- tools/allure/parent_suite.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class AllureParent_suite(str, Enum):
-    LMS = "LMS service"
-    STUDENT = "Student service"
-    ADMINISTRATION = "Administration service"
--- tools/allure/suite.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class AllureSuite(str, Enum):
-    USERS = "Users"
-    FILES = "Files"
-    COURSES = "Courses"
-    EXERCISES = "Exercises"
-    AUTHENTICATION = "Authentication"
